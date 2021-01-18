d0e67403c4b3be8c9647d52575391711975fed29
João <joao.gabsilva1007@gmail.com>
2021-01-18 17:56:43 -0300

    fixing webpack bug

--- src/pages/Details/index.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-const Details = React.FC = () => {
-    return <div />
-}
-
-export default Details
--- src/pages/Movies/index.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-const Movies = React.FC = () => {
-    return <div />
-}
-
-export default Movies
