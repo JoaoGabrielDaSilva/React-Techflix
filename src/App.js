9c841f01bc369a92e54e601179e94d82a1c39762
João <joao.gabsilva1007@gmail.com>
2021-01-18 18:12:47 -0300

    globalStyles

--- src/App.js
+++ src/App.js
@@ -6,17 +6,24 @@ import Home from './pages/Home'
 import Movie from './pages/Movie'
 import MovieDetails from './pages/MovieDetails'
 
+import GlobalStyles from './styles/GlobalStyles'
+
 function App() {
   return (
     <BrowserRouter>
 
       <Header />
 
+      <h1>Aaaaaa</h1>
+
       <Routes>
         <Route path='/' element={<Home/>} />
         <Route path='/:movie' element={<Movie/>} />
         <Route path='/:movie/details' element={<MovieDetails/>} />
       </Routes>
+
+      <GlobalStyles />
+
     </BrowserRouter>
   );
 }
---
+++ src/styles/GlobalStyles.js
@@ -0,0 +1,24 @@
+import { createGlobalStyle } from 'styled-components'
+
+export default createGlobalStyle`
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+
+    html {
+        min-height: 100%;
+        background: #ccc;
+    }
+
+    *, button, input {
+        border: 0;
+        background: none;
+        font-family: Arial, Helvetica, sans-serif
+    }
+
+    ul {
+        list-style: none;
+    }
+`
