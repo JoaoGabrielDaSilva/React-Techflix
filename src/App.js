f83d3c799e8186cd1e6261a9c126e56728fd7a41
João <joao.gabsilva1007@gmail.com>
2021-01-18 17:43:45 -0300

    techflix

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+import Header from './components/Header'
+import Home from './pages/Home'
+import Movie from './pages/Movie'
+import MovieDetails from './pages/MovieDetails'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+
+      <Header />
+
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/:movie' element={<Movie/>} />
+        <Route path='/:movie/details' element={<MovieDetails/>} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/Movies/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Movies = React.FC = () => {
+    return <div />
+}
+
+export default Movies
