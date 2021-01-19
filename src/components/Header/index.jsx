075472560871ca2f7947e25d724eda42a2837a7e
João Gabriel <joao.gabsilva1007@gmail.com>
2021-01-18 23:09:49 -0300

    Header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,7 +1,20 @@
 import React from 'react'
+import GlobalStyles from '../../styles/GlobalStyles'
+
+import { Container, Logo, Movies, Series, Search } from './styles'
 
 const Header = React.FC = () => {
-    return <div />
+    return (
+        <Container>
+            <Logo />
+            <Movies>Movies</Movies>
+            <Series>Series</Series>
+            <Search>
+                <input placeholder="Buscar filmes" />
+            </Search>
+            
+        </Container>
+    )
 }
 
 export default Header
--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -1,4 +1,68 @@
 import styled from 'styled-components'
 
 export const Container = styled.div`
+    background: #222;
+    display: flex;
+    align-items: center;
+    height: 15vh;
+    justify-content: space-between;
+
+    ul {
+        margin-left: 10px; 
+    }
+
+    ul:hover {
+        background: #4d4d4d;
+        color: #b80f0f;
+    }
+
+    form {
+        margin-right: 10px;
+    }
+
+    img {
+        margin-left: 10px
+    }
 `
+
+export const Logo = styled.img`
+    border: 1px solid #fff;
+    width: 20%;
+    height: 100px;
+    border-radius: 10px;
+    min-width: 200px;
+    max-width: 200px;
+`
+export const Movies = styled.ul`
+    outline: none;
+    color: #fff;
+    height: 100%;
+    width: 25%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`
+export const Series = styled.ul`
+    outline: none;
+    color: #fff;
+    height: 100%;
+    width: 25%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+` 
+export const Search = styled.form`
+    padding-left: 30px;
+    border-radius: 10px;
+    height: 60%;
+    width: 30%;
+    input {
+        width: 100%;
+        border-radius: 10px;
+        outline: none;
+        height: 100%;
+        border: 1px solid #fff;
+        padding: 10px;
+        color: #fff;
+    }
+` 
